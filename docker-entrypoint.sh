5b14f2295e7e79693addb98f0629324f41912378
hspotweb03 <hspot-web03@hspot.cz>
2017-06-22 09:23:10 +0200

    Update docker-entrypoint.sh
    change logging when proxy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,4 +24,36 @@ sed -i -e 's/\(.*\)\(MaxSpareServers\)\(.*\)/\1\2\t3/g' /etc/apache2/mods-enable
 sed -i -e 's/\(.*\)\(MaxRequestWorkers\)\(.*\)/\1\2\t15/g' /etc/apache2/mods-enabled/mpm_prefork.conf
 sed -i -e 's/\(.*\)\(MaxConnectionsPerChild\)\(.*\)/\1\2\t250/g' /etc/apache2/mods-enabled/mpm_prefork.conf
 
+tee /etc/apache2/sites-enabled/000-default.conf <<'EOF'
+<VirtualHost *:80>
+        # The ServerName directive sets the request scheme, hostname and port that
+        # the server uses to identify itself. This is used when creating
+        # redirection URLs. In the context of virtual hosts, the ServerName
+        # specifies what hostname must appear in the request's Host: header to
+        # match this virtual host. For the default virtual host (this file) this
+        # value is not decisive as it is used as a last resort host regardless.
+        # However, you must set it for any further virtual host explicitly.
+        #ServerName www.example.com
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+	LogFormat "%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"" combined
+        LogFormat "%{X-Forwarded-For}i %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"" proxy
+        SetEnvIf X-Forwarded-For "^.*\..*\..*\..*" forwarded
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined env=!forwarded
+        CustomLog ${APACHE_LOG_DIR}/access.log proxy env=forwarded
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+EOF
+
 exec "$@"
